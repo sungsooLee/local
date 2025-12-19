dce05f20271671c072c625340aba382e7dfe17a3
이성수 <sslee@wylie.co.kr>
2025-12-19 13:31:15 +0900

    빌드 배포 설정

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import Pages from "vite-plugin-pages";
+import { resolve } from "path";
+
+export default defineConfig({
+  plugins: [
+    Pages({
+      dirs: "src/pages", // 페이지 HTML 파일 위치
+      extensions: ["html"], // HTML 파일만 처리
+    }),
+  ],
+  root: "src", // 개발 서버 기준 폴더
+  build: {
+    outDir: "../dist", // 빌드 결과물 위치
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/pages/index.html"),
+      },
+    },
+  },
+});
